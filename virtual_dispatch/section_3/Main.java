07cd6b9988f3af33b949d4f0fb2d43416e05c697
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-02 15:22:05 -0700

    about to add object

--- virtual_dispatch/section_3/Main.java
+++ virtual_dispatch/section_3/Main.java
@@ -1,20 +1,45 @@
 public class Main {
-    // Program performs some computation
-    // Program takes command-line arguments -
-    // output destination (terminal or a file)
+    // stream is a global variable
+    // error-handling
+    // userWantsConsole and destinationFile are one unit
+    private static FileOutputStream stream = null;
+    
+    public static void writeThing(boolean userWantsConsole,
+                                  String destinationFile,
+                                  int thingToWrite) {
+        if (userWantsConsole) {
+            System.out.println(thingToWrite);
+        } else {
+            stream.writeln(thingToWrite);
+        }
+    }
+    
+    public static int doComputation(boolean userWantsConsole,
+                                    String destinationFile) {
+        int intermediateValue = 32; // some long-running computation
+
+        writeThing(userWantsConsole,
+                   destinationFile,
+                   intermediateValue);
+
+        return intermediateValue + 42;
+    }
+    
     public static void main(String[] args) {
         boolean userWantsConsole = doesUserWantConsole(args);
         String destinationFile = getDestinationFile(args);
 
-        int output = doComputation();
-
-        if (userWantsConsole) {
-            System.out.println(output);
-        } else {
+        if (!userWantsConsole) {
             File file = new File(destinationFile);
-            FileOutputStream stream = new FileOutputStream(file);
-            stream.writeln(output);
-            stream.close();
+            stream = new FileOutputStream(file);
         }
+        
+        int output = doComputation(userWantsConsole, destinationFile);
+
+        writeThing(userWantsConsole,
+                   destinationFile,
+                   output);
+
+        stream.close();
     }
 }
