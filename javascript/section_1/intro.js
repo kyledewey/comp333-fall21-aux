d80f04f1a1766d88ff9a0d0214e65a573bf1eabb
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-21 12:33:31 -0700

    added intro to JS stuff

---
+++ javascript/section_1/intro.js
@@ -0,0 +1,22 @@
+// Java:
+// // return types and parameter types required
+// public static int addTwo(int x, int y) {
+//   return x + y;
+// }
+//
+// public static String addTwo(String x, String y) {
+//   return x + y;
+// }
+
+// JavaScript:
+// No accessibility modifier (no public)
+// No return type
+// No parameter types
+//
+// More expressible than Java - more valid JavaScript programs
+// than valid Java programs.
+function addTwo(x, y) {
+    return x + y;
+}
+
+// static typing with type inference
