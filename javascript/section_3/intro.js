e1ff200e92d3f266e4dce846289ccc1288183b91
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-23 12:16:18 -0700

    code from class

---
+++ javascript/section_3/intro.js
@@ -0,0 +1,15 @@
+// Java equivalent:
+// public static int addTwo(int x, int y) {
+//   return x + y;
+// }
+//
+// public static String addTwo(String x, String y) {
+//   return x + y;
+// }
+
+// JavaScript code:
+// no return type
+// no parameter types
+function addTwo(x, y) {
+    return x + y;
+}
