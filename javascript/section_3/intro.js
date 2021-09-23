31dbaf9b13294fea31cd8178e0833fc76c66bdd5
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-23 15:29:41 -0700

    stuff from class

--- javascript/section_3/intro.js
+++ javascript/section_3/intro.js
@@ -1,4 +1,5 @@
 // Java equivalent:
+// Java is statically-typed (types exist at compile time)
 // public static int addTwo(int x, int y) {
 //   return x + y;
 // }
@@ -8,8 +9,27 @@
 // }
 
 // JavaScript code:
+// JavaScript is dynamically-typed (types exist only at runtime)
 // no return type
 // no parameter types
 function addTwo(x, y) {
+    // int temp = 17;
+    // temp = "foobar"; // type mismatch: expected int, got String
+
+    // ==: kinda equal
+    // ===: no, really, equal
+    if (typeof(x) == "number" && typeof(y) == "number") {
+        let temp = x + y;
+        return temp;
+    } else {
+        // ERROR
+    }
+}
+
+
+// Haskell: statically, strongly-typed, type-inferred
+// addTwoBasic x y = x + y
+
+function addTwoBasic(x, y) {
     return x + y;
 }
