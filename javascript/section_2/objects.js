190aa89df43ada56321084f35fa08c33e9555be1
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-30 13:30:56 -0700

    before doing the shorthand

--- javascript/section_2/objects.js
+++ javascript/section_2/objects.js
@@ -2,3 +2,31 @@
 //  look ma, no classes!
 // much more expressive model than classes
 
+// Constructors
+// constructors make an object
+// create objects with new, i.e., let r1 = new Rectangle(3, 4)
+
+let getAreaMethod = function() {
+    // for a method:
+    // this - refers to the object the method is called on
+    return this.width * this.height;
+};
+
+let rectanglePrototype = { 'getArea': getAreaMethod };
+
+function Rectangle(width, height) {
+    // for a constructor:
+    // this - refers to the object being constructed
+    this.width = width;
+    this.height = height;
+    this.__proto__ = rectanglePrototype;
+    // this.getArea = function () {
+    //     return this.width * this.height;
+    // };
+}
+
+function doSomething() {
+    for (x = 0; x < 3; x++) {
+        console.log("hello");
+    }
+}
