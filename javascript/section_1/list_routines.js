700628048ff69599930b6a8b6bee8dab5fc7a189
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-30 12:19:45 -0700

    list routines from class

---
+++ javascript/section_1/list_routines.js
@@ -0,0 +1,49 @@
+// functional programming
+//   -No loops - forced to use recursion
+
+// filter operation - grab elements that match a predicate
+// predicate: function that takes a parameter and returns a boolean
+/*function doFilter(array) {
+    for (let index = 0; index < array.length; index++) {
+        let element = array[index];
+        // if inside of a for loop - hit yes
+        if (<<some condition involving element>>) {
+            // do something else
+        }
+    }
+}
+
+function doMap(array) {
+    for (let index = 0; index < array.length; index++) {
+        let element = array[index];
+        // change element somehow
+    }
+}*/
+
+function sum(array) {
+    return doReduce(array, 0, (total, element) => total + element);
+    // let total = 0;
+    // for (let index = 0; index < array.length; index++) {
+    //     total = total + array[index];
+    // }
+    // return total;
+}
+
+function product(array) {
+    return doReduce(array, 1, (total, element) => total * element);
+    // let total = 1;
+    // for (let index = 0; index < array.length; index++) {
+    //     total = total * array[index];
+    // }
+    // return total;
+}
+
+// accumlator: starting value
+// operation: function (takes current accumulator and current array element),
+//            returns the new accumulator
+function doReduce(array, accumulator, operation) {
+    for (let index = 0; index < array.length; index++) {
+        accumulator = operation(accumulator, array[index]);
+    }
+    return accumulator;
+}
