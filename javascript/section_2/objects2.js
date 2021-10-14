9d7be9f9366a97fde35a1909c7df2806ed4ebe65
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-14 13:46:35 -0700

    stuff from today

---
+++ javascript/section_2/objects2.js
@@ -0,0 +1,28 @@
+// let rectanglePrototype = {
+//     'getArea' : function() {
+//         return this.width * this.height;
+//     }
+// };
+
+// // constructor - need to call with new
+// function Rectangle(width, height) {
+//     // this: {}
+//     this.width = width;
+//     this.height = height;
+//     this.__proto__ = rectanglePrototype;
+// }
+
+// Functions are objects
+//
+// Rectangle.prototype = {}; // done implicitly
+function Rectangle(width, height) {
+    // this.__proto__ = Rectangle.prototype; // done implicitly
+    this.width = width;
+    this.height = height;
+}
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+}
+Rectangle.prototype.getWidth = function() {
+    return this.width;
+}
