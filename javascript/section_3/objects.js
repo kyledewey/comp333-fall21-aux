fd9c2fde428473051974ec8d5421b3b987da2ab6
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-21 15:12:07 -0700

    separate prototype

--- javascript/section_3/objects.js
+++ javascript/section_3/objects.js
@@ -1,12 +1,20 @@
 // constructor
 // Rectangle = function(width, height) { ... }
+
+let rectanglePrototype = {
+    'getArea': function () {
+        return this.width * this.height;
+    }
+};
+
 function Rectangle(width, height) {
     // let this = {}; // happens implicitly
     this.width = width;
     this.height = height;
-    this.getArea = function () {
-        // in methods: `this` refers to the object we called
-        // the method on
-        return this.width * this.height;
-    };
+    this.__proto__ = rectanglePrototype;
+    // this.getArea = function () {
+    //     // in methods: `this` refers to the object we called
+    //     // the method on
+    //     return this.width * this.height;
+    // };
 }
