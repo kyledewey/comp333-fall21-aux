7bfa8f439ccc3418ff82c89e6b43ff9cad3f31f0
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-26 15:17:47 -0700

    added code from class

--- javascript/section_3/objects_shorthand.js
+++ javascript/section_3/objects_shorthand.js
@@ -1,4 +1,6 @@
+// Rectangle.prototype = {}; // happens implicitly
 function Rectangle(width, height) {
+    // this.__proto__ = Rectangle.prototype; // happens implicitly
     this.width = width;
     this.height = height;
 }
