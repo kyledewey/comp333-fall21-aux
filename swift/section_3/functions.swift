d296216740c5626af44f2e5633b85963a2926c6c
Kyle Dewey <kyle.dewey@csun.edu>
2021-11-09 15:18:16 -0800

    code from today

---
+++ swift/section_3/functions.swift
@@ -0,0 +1,57 @@
+// In Java:
+// public static int multiply(int first, int second) {
+//   return first * second;
+// }
+
+func multiply(first: Int, second: Int) -> Int {
+    return first * second
+} // multiply
+
+// Swift has semicolon inference
+
+// C++: let is basically const
+// Java: let is basically final
+let temp = multiply(first: 3, second: 4)
+// print(temp)
+
+// self-documenting code - hit yes in the chat if you've heard of it
+// Good variable/class names
+// Simpler code
+// Avoid comments
+
+func subtract(_ first: Int, _ second: Int) -> Int {
+    return first - second;
+}
+
+func divide(_ first: Int, _ second: Int) -> Int {
+    return first / second;
+}
+
+let temp2 = divide(temp, 2)
+// print(temp2)
+
+let someFunction: (Int, Int) -> Int = divide
+let temp3 = someFunction(10, 5)
+//print(temp3)
+
+func doOperation(_ b: Bool,
+                 _ f1: (Int, Int) -> Int,
+                 _ f2: (Int, Int) -> Int) -> Int {
+    if b {
+        return f1(3, 4);
+    } else {
+        return f2(3, 4);
+    }
+}
+
+let temp4 = doOperation(true, subtract, divide);
+print(temp4); // -1
+let temp5 = doOperation(false, subtract, divide);
+print(temp5); // 0
+//                                         in JavaScript
+//                                         (a, b) => a * b
+let temp6 = doOperation(false, subtract, { (a, b) in a * b });
+print(temp6);
+
+let mult/*: (Int, Int) -> Int*/ = { (a: Int, b) in a * b };
+print(mult(7, 8));
--- swift/section_3/intro.swift
+++ swift/section_3/intro.swift
@@ -1,4 +1,19 @@
-// print("Hello world")
-// type inference
-var x = 17;
-print(x);
+// let is used to introduce an immutable variable
+// var is used to introduce a mutable variable
+
+// Swift is statically typed
+let x: Int = 17; // Swift has type inference - inferred x is of type Int
+let outer = false
+if true {
+    let x: String = "foo"
+    var fromOuter = false
+    if true {
+        let x: Bool = true
+        print(x);
+        print(outer);
+        fromOuter = x
+    }
+    print(x); // prints "foo"
+}
+print(x); // prints 17
+
